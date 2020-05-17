262bab5db1d31b55774dfda4ccc358e2ef67bc82
Konano <nanoapezlk@gmail.com>
2020-05-17 19:42:09 +0800

    add flask

---
+++ backend/service.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+from flask import Flask, request, Response, jsonify
+from urllib.parse import quote, unquote, urlencode
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/api/search', methods=['POST'])
+def search():
+    ret = {
+        'total': 1,
+        'page': 0,
+        'pagesize': 20,
+        'imgURLs': [
+            '1.png',
+        ]
+    }
+    return jsonify(ret)
+
+@app.route('/api/relate', methods=['POST'])
+def relate():
+    ret = {
+        'maxsize': 20,
+        'imgURLs':[
+            '1.png',
+        ]
+    }
+    return jsonify(ret)
+
+if __name__ == '__main__':
+
+    # development
+    app.run(host='0.0.0.0', port=8388, debug=True)
+
+    # production
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8388)
