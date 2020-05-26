96b6ab8266dd322186168fd1c4e661065c8ac5e5
fnoi2014xtx <18822300009@163.com>
2020-05-26 23:30:38 +0800

    fix cbirsystem

--- backend/test_core.py
+++ backend/test_core.py
@@ -1,21 +1,18 @@
 from cbirCore import CBIRSystem, Image
-
+import os
 system = CBIRSystem()
 
 system.load_checkpoint('static/pnasnet5large-finetune500.pth')
 
 # 使用以下方法只需计算一次，之后的话加载数据使用load_data
 
-image_path = '../cbir-core/data/jpg/'
-image_ids = [
-    '132301.jpg',
-    '136002.jpg',
-    '138502.jpg',
-    '141802.jpg',
-    '146200.jpg']
-for image_id in image_ids:
-    system.load_image(Image(image_id, image_path+image_id))
-system.save_data('static/')
+# image_path = '../cbir-core/data/jpg/'
+
+# image_ids = os.listdir(image_path)
+# for image_id in image_ids:
+#     system.load_image(Image(image_id[:-4], image_path+image_id))
+#     print(image_id)
+# system.save_data('static/')
 
 # 保存之后再加载图库只需使用load
 
